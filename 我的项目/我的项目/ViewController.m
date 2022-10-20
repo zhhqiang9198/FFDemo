7f3bfa31ab14cf1ce3ce55928ba6e99cc5ff1a89
zhao <zhhqiang9198@163.com>
2022-10-20 15:51:42 +0800

    初始化项目

---
+++ 我的项目/我的项目/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  我的项目
+//
+//  Created by zhao on 2022/10/20.
+//  Copyright © 2022年 itheima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
